5011fa035263f446cef246bb1682017917a37deb
Jonas Raddatz <raddatzjonas@googlemail.com>
2019-05-20 18:43:47 +0200

    Subscribe to #

--- mqtt_client/mqtt_client.js
+++ mqtt_client/mqtt_client.js
@@ -1,16 +1,12 @@
 var mqtt = require('mqtt')
+
 var client  = mqtt.connect('mqtt://40.89.163.191:1883')
  
 client.on('connect', function () {
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }
-  })
+  client.subscribe('#') 
 })
  
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(message.toString())
-  client.end()
+  console.log(topic.toString() + ':   ' + message.toString())
 })
