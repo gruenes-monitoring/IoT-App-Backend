11cf2ede8a6765b209122f5620e9c6185f73dbf6
Jonas Raddatz <raddatzjonas@googlemail.com>
2019-05-20 20:01:46 +0200

    Klassen für Device und Measurement

---
+++ mqtt_client/device.js
@@ -0,0 +1,60 @@
+class Device {
+	constructor(topic) {
+	  var topicArr = topic.split('/');
+	  if(topicArr.length > 5 && topicArr.length < 8)
+	  {
+		  if(topicArr.length === 5) 
+		  { 
+			//both building and floor = null
+			this.country = topicArr[0];
+			this.city = topicArr[1];
+			this.address = topicArr[2];
+			this.room = topicArr[3];
+			this.id = topicArr[4];
+		  }
+		  else if(topicArr.length === 6) 
+		  {
+			//one of building and floor = null
+			this.country = topicArr[0];
+			this.city = topicArr[1];
+			this.address = topicArr[2];
+			if(isInt(topicArr[3]) {
+				this.floor = topicArr[3];
+			}				
+			else
+			{
+				this.building = topicArr[3];
+			}
+			this.room = topicArr[4];
+			this.id = topicArr[5];
+			
+		  }
+		  else 
+		  {
+			//all filled
+			if(isInt(topicArr[4]))
+			{
+				this.country = topicArr[0];
+				this.city = topicArr[1];
+				this.address = topicArr[2];
+				this.building = topicArr[3];
+				this.floor = topicArr[4];
+				this.room = topicArr[5];
+				this.id = topicArr[6];
+			}
+		  }
+	  }
+	  else 
+	  {
+		  console.log('Ungültiges Topic (Länge von ' + topicArr.length.toString() + ')');
+	  }
+	}
+}
+
+function isInt(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
+module.exports = Device;
--- mqtt_client/mqtt_client.js
+++ mqtt_client/mqtt_client.js
@@ -1,6 +1,9 @@
-var mqtt = require('mqtt')
+const Measurement = require('./measurement.js');
+const Device = require('./device.js');
 
-var client  = mqtt.connect('mqtt://40.89.163.191:1883')
+var mqtt = require('mqtt');
+
+var client  = mqtt.connect('mqtt://40.89.163.191:1883');
  
 client.on('connect', function () {
   client.subscribe('#') 
@@ -8,5 +11,6 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(topic.toString() + ':   ' + message.toString())
+  console.log(topic.toString() + ':   ' + message.toString());
+  var device = new Device(topic);
 })
