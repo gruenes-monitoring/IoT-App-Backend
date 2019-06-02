3a713a4e0c5966aad934afbef0732df1c77bf88d
Ubuntu <sa@IoT-App-DB2.n4nsvb3uod2erh52soitj5cjkg.parx.internal.cloudapp.net>
2019-06-02 22:47:31 +0000

    ups

--- graphql/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-import { mergeResolvers } from "merge-graphql-schemas";
-
-import Device from "./Device/";
-import Measurement from "./Measurement/";
-
-const resolvers = [Device,Measurement];
-
-export default mergeResolvers(resolvers);
--- graphql/types/index.js
+++
@@ -1,9 +0,0 @@
-
-import { mergeTypes } from "merge-graphql-schemas";
-
-import Measurement from "./Measurement/";
-import Device from "./Device/";
-
-const typeDefs = [Measurement, Device];
-
-export default mergeTypes(typeDefs, { all: true });
