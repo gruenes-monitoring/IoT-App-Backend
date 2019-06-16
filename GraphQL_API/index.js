88b62a6eaaa3b682ac18cbaaa7426104ebeaf7e7
Andre Matutat <andre.matutat@fh-bielefeld.de>
2019-06-16 17:24:51 +0200

    second test with GraphQL Subscriptions

--- GraphQL_API/index.js
+++ GraphQL_API/index.js
@@ -1,12 +1,15 @@
-import express from "express";
-import expressGraphQL from "express-graphql";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cors from "cors";
-import schema from "./graphql/schema";
+import express from 'express';
+import {
+  graphqlExpress,
+  graphiqlExpress,
+} from 'graphql-server-express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { schema } from "./graphql/schema";
 import { execute, subscribe } from 'graphql';
 import { createServer } from 'http';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
+import mongoose from "mongoose";
 
 const server = express();
 const PORT = process.env.PORT || "4000";
@@ -24,25 +27,30 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-const ws = createServer(server);
-ws.listen(PORT, () => {
-  console.log(`GraphQL Server is now running on http://localhost:${PORT}`);
-
-  // Set up the WebSocket for handling GraphQL subscriptions.
-  new SubscriptionServer({
-    execute,
-    subscribe,
+  
+  server.use('*', cors({ origin: 'http://localhost:3000' }));
+  server.use('/graphql', bodyParser.json(), graphqlExpress({
     schema
-  }, {
-    server: ws,
-    path: '/subscriptions',
+  }));
+  
+  server.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+  }));
+  
+  // We wrap the express server so that we can attach the WebSocket for subscriptions
+  const ws = createServer(server);
+  
+  ws.listen(PORT, () => {
+    console.log(`GraphQL Server is now running on http://localhost:${PORT}`);
+  
+    // Set up the WebSocket for handling GraphQL subscriptions
+    new SubscriptionServer({
+      execute,
+      subscribe,
+      schema
+    }, {
+      server: ws,
+      path: '/subscriptions',
+    });
   });
-});
-
-
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-  subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions` // subscriptions endpoint.
-}));
-
-
