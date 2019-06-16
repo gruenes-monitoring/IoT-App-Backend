009dcb1813c0ccc5b8ede69663d95f18e28a13de
Andre Matutat <andre.matutat@fh-bielefeld.de>
2019-06-16 15:37:18 +0200

    impl measurement request with Date

--- GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
+++ GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
@@ -5,12 +5,39 @@ import Measurement from "../../../models/Measurement";
 export default {
   Query: {
     measurementQuery: (root, args) => {
+
+      let after = false;
+      let before = false;
+      let endDate;
+      let startDate;
+      if (args.StartDate != null) {
+        after = true;
+        startDate = args.StartDate;
+        delete args.StartDate;
+      }
+      if (args.EndDate != null) {
+        before = true;
+        endDate = args.EndDate;
+        delete args.EndDate;
+      }
+
       return new Promise((resolve, reject) => {
         Measurement.find(args).exec((err, res) => {
+          if (after) {
+            for (let i = 0; i < res.length; i++) {
+              if (res[i].Timestamp < startDate) delete res[i];
+            }
+          }
+          else if (before) {
+            for (let i = 0; i < res.length; i++) {
+              if (res[i].Timestamp > endDate) delete res[i];
+            }
+          }
           err ? reject(err) : resolve(res);
         });
       });
     }
+
   },
   Mutation: {
     addMeasurement: (root, { DeviceID, Timestamp, Temperature, Humidity, Brightness }) => {
