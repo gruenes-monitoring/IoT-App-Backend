7905d19069b27d0606cfe91b744f3395afe9a43a
Ubuntu <sa@IoT-App-DB2.n4nsvb3uod2erh52soitj5cjkg.parx.internal.cloudapp.net>
2019-06-16 14:14:15 +0000

    fixed Date querys

--- GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
+++ GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
@@ -5,32 +5,34 @@ import Measurement from "../../../models/Measurement";
 export default {
   Query: {
     measurementQuery: (root, args) => {
-
       let after = false;
       let before = false;
       let endDate;
       let startDate;
-      if (args.StartDate != null) {
-        after = true;
-        startDate = args.StartDate;
-        delete args.StartDate;
+      if (args.startDate != null) {
+        after=true;
+	startDate = args.startDate;     
+        delete args.startDate;
       }
-      if (args.EndDate != null) {
+      if (args.endDate != null) {
         before = true;
-        endDate = args.EndDate;
-        delete args.EndDate;
+        endDate = args.endDate;
+        delete args.endDate;
       }
 
       return new Promise((resolve, reject) => {
-        Measurement.find(args).exec((err, res) => {
-          if (after) {
+	      Measurement.find(args).exec((err, res) => {
+          if (after) {  
             for (let i = 0; i < res.length; i++) {
-              if (res[i].Timestamp < startDate) delete res[i];
+	      if (res[i].Timestamp < startDate){
+		      delete res[i];
+	      }
+	
             }
           }
-          else if (before) {
+          if (before) {
             for (let i = 0; i < res.length; i++) {
-              if (res[i].Timestamp > endDate) delete res[i];
+              if (res[i]!=undefined && res[i].Timestamp > endDate) delete res[i];
             }
           }
           err ? reject(err) : resolve(res);
