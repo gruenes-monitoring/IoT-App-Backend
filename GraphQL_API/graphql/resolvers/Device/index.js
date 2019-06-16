bc5374ef39a28735e28974ac98478145884beea9
Ubuntu <sa@IoT-App-DB2.n4nsvb3uod2erh52soitj5cjkg.parx.internal.cloudapp.net>
2019-06-16 11:41:10 +0000

    add deviceQuery wiht objectID

--- GraphQL_API/graphql/resolvers/Device/index.js
+++ GraphQL_API/graphql/resolvers/Device/index.js
@@ -4,6 +4,8 @@ import Device from "../../../models/Device";
 export default {
   Query: {
     deviceQuery: (root, args) => {
+	//args._id="ObjectId(\""+args._id+"\")"    
+	console.log(args._id);    
       return new Promise((resolve, reject) => {
         Device.find(args).exec((err, res) => {
           err ? reject(err) : resolve(res);
