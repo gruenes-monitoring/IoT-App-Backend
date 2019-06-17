722bd3c12b8fb2efce02bdfe46f0423f87955c09
Ubuntu <sa@IoT-App-DB2.n4nsvb3uod2erh52soitj5cjkg.parx.internal.cloudapp.net>
2019-06-17 16:37:07 +0000

    fixed typo in schema

--- GraphQL_API/graphql/types/Device/types_Device.js
+++ GraphQL_API/graphql/types/Device/types_Device.js
@@ -14,11 +14,11 @@ export default `
   }
   type Query {
     deviceQuery( _id: ID, Country: String, City: String, Address: String, 
-	Buliding: String, Floor: Int, Room: String, Latitude: String, Longitude: String, Active: Boolean): [Device]
+	Building: String, Floor: Int, Room: String, Latitude: String, Longitude: String, Active: Boolean): [Device]
     
   }
   type Mutation {
   addDevice(Country: String!, City: String!, Address: String!, Description: String,
-	Buliding: String, Floor: Int, Room: String!, Latitude: String, longitude: String, Active: Boolean!): Device
+	Building: String, Floor: Int, Room: String!, Latitude: String, longitude: String, Active: Boolean!): Device
   }
 `;
