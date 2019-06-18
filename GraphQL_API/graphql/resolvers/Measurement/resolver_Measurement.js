c94f5de958146fd1b9e642f166434d31f9c24060
Andre Matutat <andre.matutat@fh-bielefeld.de>
2019-06-18 23:12:11 +0200

    enable subs without filter

--- GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
+++ GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
@@ -69,14 +69,17 @@ export default {
         () => pubsub.asyncIterator(TOPIC),
         (payload, variables) => {
           return payload.DeviceID === variables.DeviceID &&
+            (variables.MaxTemperature == undefined && variables.MinTemperature == undefined && variables.MaxBrightness == undefined && variables.MinBrightness == undefined && variables.MaxHumidity == undefined && variables.MinHumidity == undefined)
+            ||
             ((payload.Temperature != undefined && variables.MaxTemperature != undefined && payload.Temperature > variables.MaxTemperature)
-            || (payload.Temperature != undefined && variables.MinTemperature != undefined && payload.Temperature < variables.MinTemperature))
-            ||          
+              || (payload.Temperature != undefined && variables.MinTemperature != undefined && payload.Temperature < variables.MinTemperature))
+            ||
             ((payload.Humidity != undefined && variables.MaxHumidity == undefined || payload.Humidity > variables.MaxHumidity)
-            || (payload.Humidity != undefined && variables.MinHumidity == undefined || payload.Humidity < variables.MinHumidity))
+              || (payload.Humidity != undefined && variables.MinHumidity == undefined || payload.Humidity < variables.MinHumidity))
             ||
             ((payload.Brightness != undefined && variables.MaxBrightness != undefined && payload.Brightness > variables.MaxBrightness)
-            || (payload.Brightness != undefined && variables.MinBrightness != undefined && payload.Brightness < variables.MinBrightness))
+              || (payload.Brightness != undefined && variables.MinBrightness != undefined && payload.Brightness < variables.MinBrightness))
+
         }
       )
     }
