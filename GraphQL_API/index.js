e660d236b03265c9709418c0577a6e75989e224d
Ubuntu <sa@IoT-App-DB2.n4nsvb3uod2erh52soitj5cjkg.parx.internal.cloudapp.net>
2019-06-18 11:23:01 +0000

    subscriptionserver works

--- GraphQL_API/index.js
+++ GraphQL_API/index.js
@@ -5,16 +5,14 @@ import {
 } from 'graphql-server-express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { schema } from './schema';
+import schema  from './graphql/schema';
 import { execute, subscribe } from 'graphql';
 import { createServer } from 'http';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import mongoose from "mongoose";
 
-const server = express();
-const PORT = process.env.PORT || "4000";
 const db = "mongodb://127.0.0.1:27017/test";
-
+const ip= "40.89.134.226";
 
 // Connect to MongoDB with Mongoose.
 mongoose
@@ -31,22 +29,20 @@ mongoose
 const PORT = 4000;
 const server = express();
 
-server.use('*', cors({ origin: 'http://localhost:3000' }));
-
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
 }));
 
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
-  subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+subscriptionsEndpoint: `ws://${ip}:${PORT}/subscriptions`
 }));
 
 // We wrap the express server so that we can attach the WebSocket for subscriptions
 const ws = createServer(server);
 
 ws.listen(PORT, () => {
-  console.log(`GraphQL Server is now running on http://localhost:${PORT}`);
+  console.log(`GraphQL Server is now running on http://${ip}:${PORT}`);
 
   // Set up the WebSocket for handling GraphQL subscriptions
   new SubscriptionServer({
