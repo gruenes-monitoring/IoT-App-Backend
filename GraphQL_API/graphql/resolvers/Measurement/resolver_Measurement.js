bf013f6f3ea7875d4dd916fd336394cc774a6aa6
Ubuntu <sa@IoT-App-DB2.n4nsvb3uod2erh52soitj5cjkg.parx.internal.cloudapp.net>
2019-06-25 08:42:50 +0000

    finally fixed subs

--- GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
+++ GraphQL_API/graphql/resolvers/Measurement/resolver_Measurement.js
@@ -80,19 +80,20 @@ export default {
       subscribe: withFilter(
         () => pubsub.asyncIterator(TOPIC),
         (payload, variables) => {
-         myLogger.log("Subscription");
-        myLogger.group();
-        myLogger.log(new Date(Date.now())+" publish Subscription");
-        myLogger.groupEnd();       
+         myLogger.log("Subscription");	
+         myLogger.group();
+         myLogger.log(new Date(Date.now())+" publish Subscription");
+         myLogger.groupEnd();       
 
           return ((payload.DeviceID === variables.DeviceID) &&
-            ((variables.MaxTemperature === undefined && variables.MinTemperature === undefined && variables.MaxBrightness === undefined && variables.MinBrightness == undefined && variables.MaxHumidity == undefined && variables.MinHumidity == undefined)
+            ((variables.MaxTemperature === undefined && variables.MinTemperature === undefined && variables.MaxBrightness === undefined && variables.MinBrightness === undefined && variables.MaxHumidity === undefined && variables.MinHumidity === undefined)
             ||
             ((payload.Temperature != undefined && variables.MaxTemperature != undefined && payload.Temperature > variables.MaxTemperature)
               || (payload.Temperature != undefined && variables.MinTemperature != undefined && payload.Temperature < variables.MinTemperature))
             ||
-            ((payload.Humidity != undefined && variables.MaxHumidity === undefined || payload.Humidity > variables.MaxHumidity)
-              || (payload.Humidity != undefined && variables.MinHumidity === undefined || payload.Humidity < variables.MinHumidity))
+		    
+            ((payload.Humidity != undefined && variables.MaxHumidity != undefined || payload.Humidity > variables.MaxHumidity)
+              || (payload.Humidity != undefined && variables.MinHumidity != undefined || payload.Humidity < variables.MinHumidity))
             ||
             ((payload.Brightness != undefined && variables.MaxBrightness != undefined && payload.Brightness > variables.MaxBrightness)
               || (payload.Brightness != undefined && variables.MinBrightness != undefined && payload.Brightness < variables.MinBrightness))
